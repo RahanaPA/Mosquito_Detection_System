ba2b55e9c4c461b19b555772e9fc4e9b334a64dc
RahanaPA <rahanapgm@gmail.com>
2026-04-15 15:17:40 +0400

    setupfile updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup (
+    name="Mosquito_Detector",
+    version = "0.1.0",
+    author = "Rahana",
+    author_email= "rahanapgm@gmail.com",
+    packages = find_packages(),
+    install_requires= []
+
+)
